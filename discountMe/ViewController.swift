a0350093677051a7c5bf3a7a55a38b432044fed1
ariks <arik.saadi@netapp.com>
2021-06-14 12:10:40 +0300

    Initial Commit

---
+++ discountMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  discountMe
+//
+//  Created by Arik Saadi on 14/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
