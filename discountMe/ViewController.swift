6289998299b7afdf2b7125fc80543b0760fb9db2
ariks <arik.saadi@netapp.com>
2021-06-16 18:16:01 +0300

    First Commit! -Added a launch screen.

--- discountMe/ViewController.swift
+++ discountMe/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var emailTextbox: UITextField!
+    @IBOutlet weak var passwordTextbox: UITextField!
+    @IBOutlet var emailText: UIView!
+    @IBOutlet weak var passwordText: UILabel!
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+    }
+
+    @IBAction func registerButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        self.view.endEditing(true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
 }
+
 
